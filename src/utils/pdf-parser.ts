b81240b6dae303f19c49acbdcfe1b732979673b5
victorcr3709-ui <victorcr3709@gmail.com>
2026-07-02 11:59:36 -0300

    Update pdf-parser.ts

--- src/utils/pdf-parser.ts
+++ src/utils/pdf-parser.ts
@@ -32,7 +32,7 @@ const PATTERNS = {
     /(?:requerente|autor|parte[\s]requerente)[\s:]*([A-ZÁÉÍÓÚ][\w\s]+?)(?=CPF|CNPJ|requerido|endereço)/i,
   ],
   cpf: [
-    /(?:CPF|CPF/CNPJ)[\s:]*([\d]{3}\.[\d]{3}\.[\d]{3}-[\d]{2})/i,
+   /(?:CPF|CPF\/CNPJ)[\s:]*([\d]{3}\.[\d]{3}\.[\d]{3}-[\d]{2})/i,
     /([\d]{3}\.[\d]{3}\.[\d]{3}-[\d]{2})/i,
   ],
   defendantName: [
